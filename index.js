95be254ae1817fd1b22b115edbdc80a87d3732c8
Dawood-Ahmed19 <gfxwork66@gmail.com>
2023-03-30 15:57:31 +0500

    feat: made extension

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const inputEl = document.getElementById("input-el");
 const myBtn = document.getElementById("btn");
 const ulEl = document.getElementById("ul-el");
 let linksFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const deleteBtn = document.getElementById("delete-btn");
 
 if (linksFromLocalStorage) {
     myLeads = linksFromLocalStorage
     renderLead()
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+    renderLead()
+})
 
 myBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value);
@@ -20,8 +27,7 @@ function renderLead() {
 
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += `<li><a target='_blank' href='${myLeads[i]}'> ${myLeads[i]}</a></li>`
+        listItems += `<li>${myLeads[i]}</li>`
     }
     ulEl.innerHTML = listItems
 }
-
